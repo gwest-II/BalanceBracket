9261f2185352eaa4ad1fa313aa94f6ae485f4bba
gwest-II <gwest0239@gmail.com>
2022-05-28 13:09:35 +0300

    Complied

---
+++ main.py
@@ -0,0 +1,46 @@
+
+
+class Stack:
+    def __init__(self):
+        self.item = []
+
+    def isEmpty(self):
+        return self.item == []
+
+    def push(self, el):
+        self.item.append(el)
+
+    def pop(self):
+        return self.item.pop()
+
+    def peek(self):
+        return self.item[-1]
+
+    def size(self):
+        return len(self.item)
+
+
+def balance_bracket(str):
+    stack = Stack()
+    pushBracket, popBracket = '<({[', '>)}]'
+    for c in str:
+        if c in pushBracket:
+            stack.push(c)
+        elif c in popBracket:
+            if stack.isEmpty():
+                return False
+            else:
+                stack_top = stack.pop()
+                balancing_bracket = pushBracket[popBracket.index(c)]
+                if stack_top != balancing_bracket:
+                    return "Несбалансированно"
+        else:
+            return "Несбалансированно"
+    else:
+        return "Сбалансированно"
+
+
+
+if '__main__' == __name__:
+
+    print(balance_bracket('()'))
